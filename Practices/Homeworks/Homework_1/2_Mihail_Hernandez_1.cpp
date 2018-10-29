8e2bca0320691ad341a42918d4cdc1e05c9eec13
mihailhernandez <mihailernandes@gmail.com>
2018-10-29 17:24:48 +0200

    Create separate folder for the practice lessons.

---
+++ Practices/Homeworks/Homework_1/2_Mihail_Hernandez_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/** 
+ * Name:           Mihail Hernandez
+ * Faculty number: 45564
+ * Programme:      Informatics
+ * Group:          3
+ */
+
+/**
+ * Celsuis to fahrenheit. 
+ */
+int main()
+{
+	double celsius;
+
+	std::cin >> celsius;
+	std::cout << celsius * (1.8) + 32 << std::endl;
+
+	return 0;
+}
+
---
+++ Practices/Homeworks/Homework_1/3_Mihail_Hernandez_1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+/** 
+ * Name:           Mihail Hernandez
+ * Faculty number: 45564
+ * Programme:      Informatics
+ * Group:          3
+ */
+
+/**
+ * You can fry a given number of bread slices on a flying pan,
+ * each side taking to fry for a given amount of time. Having
+ * the number of slices, how much time will it take to fry all 
+ * of the bread slices.
+ */ 
+int main()
+{
+	unsigned short times_to_fly_slices;
+	unsigned short number_of_slices;
+        unsigned short time_to_fry_side;
+	unsigned short pan_slice_capacity;
+
+	std::cout << "Enter pan capacity: ";
+	std::cin >> pan_slice_capacity;
+	std::cout << "Enter time to cook each slice: ";
+	std::cin >> time_to_fry_side;
+	std::cout << "Enter number of slices: ";
+	std::cin >> number_of_slices;
+
+	times_to_fly_slices = (number_of_slices % pan_slice_capacity > 0)
+		? number_of_slices * 2 / pan_slice_capacity + 1
+		: number_of_slices * 2 / pan_slice_capacity;
+	
+	std::cout << times_to_fly_slices * time_to_fry_side << std::endl;
+	return 0;
+}
+
